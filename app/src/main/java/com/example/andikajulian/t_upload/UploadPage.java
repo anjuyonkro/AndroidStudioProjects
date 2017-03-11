7de5fb9951511061a201b3d828d0ff9800f659a9
anjuyonkro <anjuyonkro176@gmail.com>
2017-03-11 20:23:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/andikajulian/t_upload/UploadPage.java
@@ -0,0 +1,13 @@
+package com.example.andikajulian.t_upload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_page);
+    }
+}
